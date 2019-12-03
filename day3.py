05ecc7066846a6d162f99045a13a988d17aaca70
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-03 10:53:08 -0500

    Adds framework for day3

---
+++ day3.py
@@ -0,0 +1,9 @@
+# generate a grid with empty strings
+# 
+
+# two problems
+# 1. Determine where the two lines cross
+# 2. Determine which crossing is closer to the origin
+
+# Determine the size of the grid = Combination of all ups, downs, lefts, and rights
+
