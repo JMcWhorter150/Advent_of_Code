ce677bdcb121dba19713bb160c6b5424c4ab8657
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-03 13:04:28 -0500

    Adds the ability to create a matrix and print a matrix

--- day3.py
+++ day3.py
@@ -1,5 +1,22 @@
-# generate a grid with empty strings
-# 
+# generate a grid with empty strings of any size
+def create_matrix(width, height):
+    # create empty matrix
+    empty_matrix = []
+    # create columns/width
+    empty_row = []
+    while len(empty_row) < width:
+        empty_row += " "
+    while len(empty_matrix) < height:
+        empty_matrix.append(empty_row)
+    return empty_matrix
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+matrix = create_matrix(7, 7)
+print_matrix(matrix)
+
 
 # two problems
 # 1. Determine where the two lines cross
